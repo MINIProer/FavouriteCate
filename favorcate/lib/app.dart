12b0f732ba60a7d2489201038e4055cde1bf3630
jiangshulun <jiangshulun@lanjinger.com>
2020-04-07 15:01:14 +0800

    *) 抽取封装runApp中相关代码

---
+++ favorcate/lib/app.dart
@@ -0,0 +1,14 @@
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '美食广场 ',
+      theme: JRAppTheme.normalThemeData, // 主题
+      initialRoute: JRRouter.initialRoute, // 默认启动页面
+      routes: JRRouter.routers, // 路由映射关系
+      onGenerateRoute: JRRouter.generateRoute, // 路由钩子函数
+      onUnknownRoute: JRRouter.unknowRoute, // 空页面or未知页面
+    );
+  }
+}
--- favorcate/lib/main.dart
+++ favorcate/lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:favorcate/core/router/router.dart';
-import 'package:favorcate/ui/shared/app_theme.dart';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'app.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '美食广场 ',
-      theme: JRAppTheme.normalThemeData,
-      initialRoute: JRRouter.initialRoute, // 默认启动页面
-      routes: JRRouter.routers, // 路由映射关系
-      onGenerateRoute: JRRouter.generateRoute, // 路由钩子函数
-      onUnknownRoute: JRRouter.unknowRoute, // 空页面or未知页面
-    );
-  }
-}
+void main() => runApp(MyApp());
