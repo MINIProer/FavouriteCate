d2f1a712c4e0bc75987be5120c68be5e7127ff81
jiangshulun <jiangshulun@lanjinger.com>
2020-04-09 17:51:03 +0800

    *) 完善抽屉页面相关交互

--- favorcate/lib/ui/pages/home/home_drawer.dart
+++ favorcate/lib/ui/pages/home/home_drawer.dart
@@ -1,3 +1,4 @@
+
 import 'package:favorcate/ui/widgets/drawer_menu_item.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +12,7 @@ class JRHomeDrawer extends StatelessWidget {
       color: Theme.of(context).canvasColor,
       child: Drawer(
         child: Column(
-          children: <Widget>[
-            setupTopTitleArea(),
-            setupMenuArea()
-          ],
+          children: <Widget>[setupTopTitleArea(), setupMenuArea(context)],
         ),
       ),
     );
@@ -34,19 +32,27 @@ class JRHomeDrawer extends StatelessWidget {
         ));
   }
 
-  Widget setupMenuArea() {
+  Widget setupMenuArea(BuildContext context) {
     return Padding(
       padding: EdgeInsets.fromLTRB(15.px, 20.px, 15.px, 20.px),
       child: Column(
         children: <Widget>[
           JRDrawerMenuItem(
             Icons.restaurant,
-            '进餐'
+            '进餐',
+            handler: () {
+              print('进餐');
+              Navigator.of(context).pop();
+            },
           ),
           SizedBox(height: 20.px),
           JRDrawerMenuItem(
             Icons.settings,
-            '设置'
+            '设置',
+            handler: () {
+              print('设置');
+              Navigator.of(context).pop();
+            },
           )
         ],
       ),
--- favorcate/lib/ui/widgets/drawer_menu_item.dart
+++ favorcate/lib/ui/widgets/drawer_menu_item.dart
@@ -5,24 +5,27 @@ import 'package:favorcate/core/extensions/int_extension.dart';
 class JRDrawerMenuItem extends StatelessWidget {
   final IconData _iconData;
   final String _menuTitle;
-
-  JRDrawerMenuItem(this._iconData, this._menuTitle);
+  final Function handler;
+  JRDrawerMenuItem(this._iconData, this._menuTitle, {this.handler});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      color: Theme.of(context).canvasColor,
-      child: Row(
-        children: <Widget>[
-          Icon(_iconData, size: 20.px, color: Colors.grey[500]),
-          SizedBox(width: 20.px),
-          Text(
-            _menuTitle,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          )
-        ],
+    return GestureDetector(
+      child: Container(
+        width: double.infinity,
+        color: Theme.of(context).canvasColor,
+        child: Row(
+          children: <Widget>[
+            Icon(_iconData, size: 20.px, color: Colors.grey[500]),
+            SizedBox(width: 20.px),
+            Text(
+              _menuTitle,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
       ),
+      onTap: handler,
     );
   }
 }
